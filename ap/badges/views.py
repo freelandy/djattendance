b989efc889281622870e34412637078cc9afb56b
Plew <plewwatson@gmail.com>
2018-12-21 17:15:55 -0800

    Strange newline complain on git, trying to fix

--- ap/badges/views.py
+++ ap/badges/views.py
@@ -630,4 +630,3 @@ class BadgePrintSettingsUpdateView(BadgesGroupRequiredMixin, UpdateView):
   def get_context_data(self, **kwargs):
     context = super(BadgePrintSettingsUpdateView, self).get_context_data(**kwargs)
     return context
-  
