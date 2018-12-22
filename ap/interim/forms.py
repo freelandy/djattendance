4c8ef576522132bef3518ba617b60c0a4740dcf8
bdou <bridget.dou@gmail.com>
2018-12-21 17:29:48 -0800

    added more itinerary logic; found a bug with event firing.

--- ap/interim/forms.py
+++ ap/interim/forms.py
@@ -14,6 +14,7 @@ class InterimItineraryForm(forms.ModelForm):
     model = InterimItinerary
     fields = ["start", "end", "comments", ]
     widgets = {
+      "start": DatePicker(),
       "end": DatePicker(),
       "comments": forms.Textarea(attrs={'rows': 2})
     }
